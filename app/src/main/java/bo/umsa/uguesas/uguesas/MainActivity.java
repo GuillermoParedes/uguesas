f1e34d6f579aabd13b0724d8be640cec67470a92
GuillermoParedes <gdavid.ptorrez@gmail.com>
2015-04-15 18:08:01 -0400

    Commit 4

--- app/src/main/java/bo/umsa/uguesas/uguesas/MainActivity.java
+++ app/src/main/java/bo/umsa/uguesas/uguesas/MainActivity.java
@@ -1,5 +1,6 @@
 package bo.umsa.uguesas.uguesas;
 
+import android.provider.SyncStateContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -34,44 +35,9 @@ public class MainActivity extends ActionBarActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch(v.getId()) {
-                    case R.id.b1:
-                        // it was the first button
-                        Toast.makeText(getApplication(),"R.id.b1", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b2:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b2", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b3:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b3", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b4:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b4", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b5:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b5", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b6:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b6", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b7:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b7", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b8:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b8", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.b9:
-                        // it was the second button
-                        Toast.makeText(getApplication(),"R.id.b9", Toast.LENGTH_SHORT).show();
-                        break;
-                }
+                String nombre = "Guillermo";
+                String apellido = "Paredes";
+                Bundle bundle = new Bundle();
             }
         });
 
