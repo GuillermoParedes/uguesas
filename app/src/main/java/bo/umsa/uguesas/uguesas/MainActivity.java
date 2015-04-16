a6d79ce5f98eaef0e9b90e54d9a6508b9417cb55
GuillermoParedes <gdavid.ptorrez@gmail.com>
2015-04-16 16:58:29 -0400

    Commit 6

--- app/src/main/java/bo/umsa/uguesas/uguesas/MainActivity.java
+++ app/src/main/java/bo/umsa/uguesas/uguesas/MainActivity.java
@@ -1,6 +1,5 @@
 package bo.umsa.uguesas.uguesas;
 
-import android.provider.SyncStateContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,13 +15,22 @@ public class MainActivity extends ActionBarActivity {
             R.drawable.hconfideitos, R.drawable.hconhuevo, R.drawable.hdoble,
             R.drawable.hgemelas,R.drawable.hmariajuana, R.drawable.hmediokilo,
             R.drawable.hnormal, R.drawable.hpollo, R.drawable.hvegetal};
-    ImageButton b1, b2, b3,b4,b5,b6,b7,b8,b9;
-
-
+    ImageButton b1;
+    ImageButton b2;
+    ImageButton b3;
+    ImageButton b4;
+    ImageButton b5;
+    ImageButton b6;
+    ImageButton b7;
+    ImageButton b8;
+    ImageButton b9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mostrarHambur(View view){
         b1 = (ImageButton) findViewById(R.id.b1);
         b2 = (ImageButton) findViewById(R.id.b2);
         b3 = (ImageButton) findViewById(R.id.b3);
@@ -32,40 +40,37 @@ public class MainActivity extends ActionBarActivity {
         b7 = (ImageButton) findViewById(R.id.b7);
         b8 = (ImageButton) findViewById(R.id.b8);
         b9 = (ImageButton) findViewById(R.id.b9);
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nombre = "Guillermo";
-                String apellido = "Paredes";
-                Bundle bundle = new Bundle();
-            }
-        });
-
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        switch(view.getId()) {
+                    case R.id.b1:
+                        Toast.makeText(getApplication(),"R.id.b1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b2:
+                        Toast.makeText(getApplication(),"R.id.b2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b3:
+                        Toast.makeText(getApplication(),"R.id.b3", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b4:
+                        Toast.makeText(getApplication(),"R.id.b4", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b5:
+                        Toast.makeText(getApplication(),"R.id.b5", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b6:
+                        Toast.makeText(getApplication(),"R.id.b6", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b7:
+                        Toast.makeText(getApplication(),"R.id.b7", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b8:
+                        Toast.makeText(getApplication(),"R.id.b8", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.b9:
+                        Toast.makeText(getApplication(),"R.id.b9", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
     }
 
 
