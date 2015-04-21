fabcb96043318ef8cdfff76b29f0f003b551fcdf
GuillermoParedes <gdavid.ptorrez@gmail.com>
2015-04-21 15:09:00 -0400

    Commit 9

--- app/src/main/java/bo/umsa/uguesas/uguesas/SecondActivity.java
+++ app/src/main/java/bo/umsa/uguesas/uguesas/SecondActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewDebug;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,6 +28,11 @@ public class SecondActivity extends ActionBarActivity {
     String parametros;
     ImageView img;
     TextView title;
+    Button agregar;
+    Button quitar;
+    int cantidad;
+    int pr;
+    int precioham;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +43,10 @@ public class SecondActivity extends ActionBarActivity {
         Bundle b = in.getExtras();
 
         parametros = b.getString("parametro");
-
-
-
-
         hamburguesa(parametros);
+
+        cantidad = 0;
+        pr = 0;
     }
 
 
@@ -76,49 +83,74 @@ public class SecondActivity extends ActionBarActivity {
                 img.setImageResource(hamburguesas[0]);
                 precio.setText("PRECIO : Bs."+ precios[0]);
                 title.setText("HAMBURGUESA CON FIDEO");
+                precioham = 15;
                 break;
             case "Button B2":
                 img.setImageResource(hamburguesas[1]);
                 precio.setText("PRECIO : Bs."+ precios[1]);
                 title.setText("HAMBURGUESA CON HUEVO");
+                precioham = 14;
                 break;
             case "Button B3":
                 img.setImageResource(hamburguesas[2]);
                 precio.setText("PRECIO : Bs. "+ precios[2]);
                 title.setText("HAMBURGUESA DOBLE");
+                precioham = 18;
                 break;
             case "Button B4":
                 img.setImageResource(hamburguesas[3]);
                 precio.setText("PRECIO : Bs. "+ precios[3]);
                 title.setText("HAMBURGUESA GEMELAS");
+                precioham = 21;
                 break;
             case "Button B5":
                 img.setImageResource(hamburguesas[4]);
                 precio.setText("PRECIO : Bs. "+ precios[4]);
                 title.setText("HAMBURGUESA MARIJUANA");
+                precioham = 25;
                 break;
             case "Button B6":
                 img.setImageResource(hamburguesas[5]);
                 precio.setText("PRECIO : Bs. "+ precios[5]);
                 title.setText("HAMBURGUESA 1/2 CARNE");
+                precioham = 18;
                 break;
             case "Button B7":
                 img.setImageResource(hamburguesas[6]);
                 precio.setText("PRECIO : Bs. "+ precios[6]);
                 title.setText("HAMBURGUESA NORMAL");
+                precioham = 12;
                 break;
             case "Button B8":
                 img.setImageResource(hamburguesas[7]);
                 precio.setText("PRECIO : Bs. "+ precios[7]);
                 title.setText("HAMBURGUESA DE POLLO");
+                precioham = 12;
                 break;
             case "Button B9":
                 img.setImageResource(hamburguesas[8]);
                 precio.setText("PRECIO : Bs. "+ precios[8]);
                 title.setText("HAMBURGUESA VEGETARIANA");
+                precioham = 15;
                 break;
             default:
                 break;
         }
+    }
+
+    public void agregar(View view){
+        TextView can = (TextView) findViewById(R.id.cantidad);
+        TextView precio = (TextView) findViewById(R.id.precio);
+
+//        int nc = Integer.parseInt((String) cantidad.getText());
+//        nc = nc +1;
+
+        cantidad +=1;
+
+        pr = pr + precioham;
+
+        can.setText(String.valueOf(cantidad));
+        precio.setText(String.valueOf(pr));
+
     }
 }
